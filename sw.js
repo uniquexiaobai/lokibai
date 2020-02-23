dfbe84d97858e62a46d9ea55cde465c55cfd4f47
uniquexiaobai <uniquexiaobai@gmail.com>
2020-02-23 16:02:52 +0800

    add routes

--- sw.js
+++ sw.js
@@ -1,80 +1,82 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+importScripts(
+	'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
+);
 
 workbox.routing.registerRoute(
 	new RegExp('/(index.html)?$'),
-	new workbox.strategies.StaleWhileRevalidate(),
+	new workbox.strategies.StaleWhileRevalidate()
 );
 
 workbox.routing.registerRoute(
 	/^https:\/\/bing-api\.lokibai\.com/,
-	new workbox.strategies.StaleWhileRevalidate(),
+	new workbox.strategies.StaleWhileRevalidate()
 );
 
 workbox.routing.registerRoute(
 	/^https:\/\/cn\.bing\.com/,
-	new workbox.strategies.StaleWhileRevalidate(),
+	new workbox.strategies.StaleWhileRevalidate()
 );
 
 workbox.routing.registerRoute(
-	/^https:\/\/uniquexiaobai\.cn/,
+	/^https:\/\/uniquexiaobai\.oss-cn-hangzhou\.aliyuncs\.com/,
 	new workbox.strategies.CacheFirst({
-        cacheName: 'images',
+		cacheName: 'images',
 		plugins: [
-	        new workbox.cacheableResponse.Plugin({
-	            statuses: [0, 200]
-            }),
-            new workbox.expiration.Plugin({
-                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-                maxEntries: 30,
-            }),
-	    ],
-	}),
+			new workbox.cacheableResponse.Plugin({
+				statuses: [0, 200],
+			}),
+			new workbox.expiration.Plugin({
+				maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+				maxEntries: 30,
+			}),
+		],
+	})
 );
 
 workbox.routing.registerRoute(
 	/^https:\/\/fonts\.gstatic\.com/,
 	new workbox.strategies.CacheFirst({
-        cacheName: 'fonts',
+		cacheName: 'fonts',
 		plugins: [
-	        new workbox.cacheableResponse.Plugin({
-	            statuses: [0, 200]
-            }),
-            new workbox.expiration.Plugin({
-                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-                maxEntries: 30,
-            }),
-	    ],
-	}),
+			new workbox.cacheableResponse.Plugin({
+				statuses: [0, 200],
+			}),
+			new workbox.expiration.Plugin({
+				maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+				maxEntries: 30,
+			}),
+		],
+	})
 );
 
 workbox.routing.registerRoute(
-    new RegExp('/icon'),
+	new RegExp('/icon'),
 	new workbox.strategies.CacheFirst({
-        cacheName: 'manifest',
+		cacheName: 'manifest',
 		plugins: [
-	        new workbox.cacheableResponse.Plugin({
-	            statuses: [0, 200]
-            }),
-            new workbox.expiration.Plugin({
-                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
-                maxEntries: 30,
-            }),
-	    ],
-	}),
+			new workbox.cacheableResponse.Plugin({
+				statuses: [0, 200],
+			}),
+			new workbox.expiration.Plugin({
+				maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+				maxEntries: 30,
+			}),
+		],
+	})
 );
 
 workbox.routing.registerRoute(
-    new RegExp('/manifest.json'),
+	new RegExp('/manifest.json'),
 	new workbox.strategies.CacheFirst({
-        cacheName: 'manifest',
+		cacheName: 'manifest',
 		plugins: [
-	        new workbox.cacheableResponse.Plugin({
-	            statuses: [0, 200]
-            }),
-            new workbox.expiration.Plugin({
-                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
-                maxEntries: 30,
-            }),
-	    ],
-	}),
+			new workbox.cacheableResponse.Plugin({
+				statuses: [0, 200],
+			}),
+			new workbox.expiration.Plugin({
+				maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+				maxEntries: 30,
+			}),
+		],
+	})
 );
