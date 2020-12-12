6c69ab086561900d6cde5dfddc9dab1722e4846e
uniquexiaobai <uniquexiaobai@gmail.com>
2020-12-12 16:54:17 +0800

    fix: 更新域名

--- sw.js
+++ sw.js
@@ -1,82 +1,98 @@
 importScripts(
-	'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
+  'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
 );
 
 workbox.routing.registerRoute(
-	new RegExp('/(index.html)?$'),
-	new workbox.strategies.StaleWhileRevalidate()
+  new RegExp('/(index.html)?$'),
+  new workbox.strategies.StaleWhileRevalidate()
 );
 
 workbox.routing.registerRoute(
-	/^https:\/\/bing-api\.lokibai\.com/,
-	new workbox.strategies.StaleWhileRevalidate()
+  /^https:\/\/bing-api\.lokibai\.vercel\.app/,
+  new workbox.strategies.StaleWhileRevalidate()
 );
 
 workbox.routing.registerRoute(
-	/^https:\/\/cn\.bing\.com/,
-	new workbox.strategies.StaleWhileRevalidate()
+  /^https:\/\/cn\.bing\.com/,
+  new workbox.strategies.StaleWhileRevalidate()
 );
 
 workbox.routing.registerRoute(
-	/^https:\/\/uniquexiaobai\.oss-cn-hangzhou\.aliyuncs\.com/,
-	new workbox.strategies.CacheFirst({
-		cacheName: 'images',
-		plugins: [
-			new workbox.cacheableResponse.Plugin({
-				statuses: [0, 200],
-			}),
-			new workbox.expiration.Plugin({
-				maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-				maxEntries: 30,
-			}),
-		],
-	})
+  /^https:\/\/uniquexiaobai\.oss-cn-hangzhou\.aliyuncs\.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+        maxEntries: 30,
+      }),
+    ],
+  })
 );
 
 workbox.routing.registerRoute(
-	/^https:\/\/fonts\.gstatic\.com/,
-	new workbox.strategies.CacheFirst({
-		cacheName: 'fonts',
-		plugins: [
-			new workbox.cacheableResponse.Plugin({
-				statuses: [0, 200],
-			}),
-			new workbox.expiration.Plugin({
-				maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-				maxEntries: 30,
-			}),
-		],
-	})
+  /^https:\/\/cdnjs\.cloudflare\.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'fonts',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+        maxEntries: 30,
+      }),
+    ],
+  })
 );
 
 workbox.routing.registerRoute(
-	new RegExp('/icon'),
-	new workbox.strategies.CacheFirst({
-		cacheName: 'manifest',
-		plugins: [
-			new workbox.cacheableResponse.Plugin({
-				statuses: [0, 200],
-			}),
-			new workbox.expiration.Plugin({
-				maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
-				maxEntries: 30,
-			}),
-		],
-	})
+  /^https:\/\/fonts\.gstatic\.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'fonts',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+        maxEntries: 30,
+      }),
+    ],
+  })
 );
 
 workbox.routing.registerRoute(
-	new RegExp('/manifest.json'),
-	new workbox.strategies.CacheFirst({
-		cacheName: 'manifest',
-		plugins: [
-			new workbox.cacheableResponse.Plugin({
-				statuses: [0, 200],
-			}),
-			new workbox.expiration.Plugin({
-				maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
-				maxEntries: 30,
-			}),
-		],
-	})
+  new RegExp('/icon'),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'manifest',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp('/manifest.json'),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'manifest',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+        maxEntries: 30,
+      }),
+    ],
+  })
 );
